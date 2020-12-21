8b83c0c2034f0bc99af3caeec48ee9672c386421
Wojciech <w.szajnerman@gmail.com>
2020-12-21 11:38:06 +0100

    Add files via upload

---
+++ libTagVersion.h
@@ -0,0 +1,29 @@
+// ================================================== library/BM8563/libTagVersion.h ==
+// Name:	libTagVersion.h
+// Version:	0.0.1
+// Desc.:	Version of the BM8563 library.
+//
+// Licence:	Permission is hereby granted, free of charge, to any person obtaining
+//			a copy of this software and associated documentation files (the
+// 			"Software"), to deal in the Software without restriction, including
+//			without limitation the rights to use, copy, modify, merge, publish,
+//			distribute, sublicense, and/or sell copies of the Software, and to permit
+//			persons to whom the Software is furnished to do so, subject to the
+//			following conditions: This permission notice shall be included in all
+//			copies or substantial portions of the Software.
+//
+//			THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//			IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//			FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+//			THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//			LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+//			FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+//			DEALINGS IN THE SOFTWARE.
+// ================================================== library/BM8563/libTagVersion.h ==
+
+#define BM8563_LIB_VERSION					(F("0.0.1"))
+
+// ====================================================================================
+// Date:	21.12.2020
+// Author:	(©) 2020 Wojciech Szajnerman <w.szajnerman@gmail.com>
+// =========================================== end of library/BM8563/libTagVersion.h ==
